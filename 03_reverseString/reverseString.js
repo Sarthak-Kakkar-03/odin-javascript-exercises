6225c5dbbdac7577edf4c71c04aeaec8e4869e82
Sarthak-Kakkar-03 <sarthakkakkar2021@gmail.com>
2025-04-23 15:25:10 -0400

    ex3 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    result = '';
+    for (let i = str.length; i >= 0; i--) {
+        result += str.charAt(i);
+    }
+    return result;
 };
 
 // Do not edit below this line
