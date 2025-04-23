349a6c2da585ca600f396bc7bd3b41f21e7be56b
Sarthak-Kakkar-03 <sarthakkakkar2021@gmail.com>
2025-04-23 16:08:49 -0400

    ex 7 done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
 const leapYears = function() {
-
+let num = arguments[0];
+if (num % 100 === 0) {
+    return num % 400 === 0;
+}
+else {
+    return num % 4 === 0;
+}
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
 const convertToCelsius = function() {
+  let farenheitVal = arguments[0];
+  return Math.round(((farenheitVal - 32) * 5/9) * 10)/10;
 };
 
 const convertToFahrenheit = function() {
+  let celciusVal = arguments[0];
+  return Math.round(((celciusVal * (9/5)) + 32) * 10) /10;
 };
 
 // Do not edit below this line
