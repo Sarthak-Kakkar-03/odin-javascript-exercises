fdb6e6e0385ff377fe38a83ead0934dd9b7cb9e3
Sarthak-Kakkar-03 <sarthakkakkar2021@gmail.com>
2025-04-23 15:40:34 -0400

    ex4 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
 const removeFromArray = function() {
+    let array = arguments[0];
+    for (let i = 1; i < arguments.length; i++) {
+        let idx = array.indexOf(arguments[i]);
+        while (idx !== -1) {
+            array.splice(idx, 1);
+            idx = array.indexOf(arguments[i]);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
