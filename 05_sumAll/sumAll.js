b2a64e71f2096c672f2ef7f9f4ffeee75423d473
Sarthak-Kakkar-03 <sarthakkakkar2021@gmail.com>
2025-04-23 15:50:27 -0400

    ex 5 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
 const sumAll = function() {
+    result = 0;
+    if (!(Number.isInteger(arguments[0]) && Number.isInteger(arguments[1]))) {
+        return 'ERROR';
+    } else if (arguments[0] < 0 || arguments[1] < 0) {
+        return 'ERROR';
+    }
+    let smaller_num = -1;
+    let larger_num = -1;
+    if (arguments[0] > arguments[1]) {
+         larger_num = arguments[0];
+         smaller_num = arguments[1];
+    } else {
+         larger_num = arguments[1];
+         smaller_num = arguments[0];
+    }
+    for (let i = smaller_num; i <= larger_num; i++) {
+        result += i;
+    }
+    return result;
 
 };
 
