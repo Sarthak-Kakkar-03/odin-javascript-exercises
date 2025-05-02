e25f744de884c6431744a082b671909c237701d5
Sarthak-Kakkar-03 <sarthakkakkar2021@gmail.com>
2025-05-02 00:26:58 -0400

    ex 8 done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
 const add = function() {
-	
+	return arguments[0] + arguments[1];
 };
 
 const subtract = function() {
-	
+	return arguments[0] - arguments[1];
 };
 
 const sum = function() {
-	
+  const args = arguments[0];
+	return args
+  .reduce(
+    (accumulator, currentVal) => accumulator + currentVal, 0
+  )
 };
 
 const multiply = function() {
-
+  const args = arguments[0];
+	return args
+  .reduce(
+    (accumulator, currentVal) => accumulator * currentVal
+  )
 };
 
 const power = function() {
-	
+	return arguments[0] ** arguments[1]
 };
-
 const factorial = function() {
-	
+  if (arguments.length === 0 || arguments[0] === 0) {
+    return 1;
+  }
+  let result = 1;
+  for (let i = arguments[0]; i > 0; i--) {
+    result = result * i;
+  }
+  return result;
 };
 
 // Do not edit below this line
